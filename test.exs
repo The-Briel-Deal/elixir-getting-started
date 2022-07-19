31518b6e17bd30782394278c2270e9d76fa8603a
The-Briel-Deal <gabe@fordltc.net>
2022-07-18 22:46:10 -0400

    Playing with cases

--- test.exs
+++ test.exs
@@ -1,2 +1,11 @@
 person = IO.gets "What is your name?"
-IO.puts "Hello #{person}!"
+case person do
+  "Yaya\n" ->
+    IO.puts "Yaya likes dogs!"
+  "Gabriel\n" ->
+    IO.puts "Gabriel likes kratom!!"
+  "Austin\n" ->
+    IO.puts "Austin likes vaping!!!"
+  _ ->
+    IO.puts "DID I TELL YOU TO SAY THAT!!!"
+end
